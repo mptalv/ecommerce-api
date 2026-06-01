98ab0e51ccae62875dcf36622d4b6fa49961fb66
mptalv <mptalv89@gmail.com>
2026-06-01 17:01:11 -0400

    updated with JWT

--- auth.py
+++ auth.py
@@ -1,12 +1,27 @@
 from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+from jose import jwt
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password):
+SECRET_KEY = "supersecretkey"  # later move to .env
+ALGORITHM = "HS256"
+
+
+# PASSWORDS
+def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(password, hashed):
+
+def verify_password(password: str, hashed: str):
     return pwd_context.verify(password, hashed)
+
+
+# JWT
+def create_access_token(data: dict, expires_minutes: int = 60):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_minutes)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
