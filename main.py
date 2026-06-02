e316529cde9ea7f5abb6dbf514401d454dd968a7
mptalv <mptalv89@gmail.com>
2026-06-02 17:24:54 -0400

    include routers

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from database import engine, Base
 
+from routers import auth
+from routers import users
+
 from models.user import User 
 
 app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(users.router)
 
 Base.metadata.create_all(bind=engine)
 
